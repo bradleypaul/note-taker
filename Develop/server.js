23a84d9a18283da758fdaac363650ab4bff789b0
Paul Bradley <bradleypauld@gmail.com>
2020-05-30 13:54:13 -0500

    remove db functions from server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,33 +1,52 @@
 const express = require('express');
-const fs = require('fs');
+
+// uniqid makes unique ids off of time, package, and process.
+// should be very slim chance that this generates two identical ids
+const uniqid = require('uniqid');
+
+// body parser for req.body
+const bodyParser = require('body-parser');
+
+const {get, set} = require('./db');
 
 const app = express();
 const port = 8080;
 
-app.use(express.static(`${__dirname}`))
+app.use(express.static(`${__dirname}`));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/notes', async (req, res) => {
-    console.log(`${__dirname}/public/index.html`)
     res.sendFile(`${__dirname}/public/notes.html`);
 });
 
-app.get('/api/notes', async(req, res) => {
-    console.log("Got DB");
-    const json = await fs.promises.readFile('./db/db.json', 'utf-8');
-    // console.log()
-    res.send(JSON.parse(json));
+app.get('/api/notes', async (req, res) => {
+    res.send(await get());
 });
 
-app.post('/api/notes', async(req, res) => {
-    res.send (await fs.promises.readFile('./db/db.json', 'utf-8'));
+app.post('/api/notes', async (req, res) => {
+    const note = req.body;
+    note.id = uniqid();
+    const notesDB = await get();
+    // combine db with ne note
+    await set([...notesDB, note])
+
+    // send result to api
+    res.send(note);
 });
+
+app.delete('/api/notes/:noteId', async (req, res) => {
+    const notesDB = (await get()).filter(item => item.id === req.params.id);
+
+    await set(notesDB);
+
+    // send 204 because it's ok, no need to send a payload though.
+    res.sendStatus(204); 
+}) 
 
 app.get('*', async (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`);
 });
 
-
-
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`)
-})
+});
